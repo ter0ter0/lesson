795b39764532bf7515330beaa113fa42dec2a170
ter0ter0 <panicdepanic@gmail.com>
2024-05-11 22:32:38 +0000

    first_commit

---
+++ PW_MANAGER.sh
@@ -0,0 +1,14 @@
+echo "パスワードマネージャーへようこそ！"
+
+read -p  "サービス名を入力してください:"  service_name
+
+read -p "ユーザー名を入力してください"  user_name
+
+read -p "パスワードを入力してください" pasward
+
+
+echo "サービス名:$servis_name" >>pasward.txt
+
+
+
+echo "Thanks you"
