1ec71bad5215518c440fefaf3d98befc1fe004c5
ter0ter0 <panicdepanic@gmail.com>
2024-05-12 00:07:15 +0000

    pw.sh

---
+++ pw.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "パスワードマネージャーへようこそ！"
+
+read -p  "サービス名を入力してください:" service_name
+
+read -p "ユーザー名を入力してください"  user_name
+
+read -p "パスワードを入力してください" passward
+
+
+echo ${service_name} >>service_name.txt
+echo ${user_name}>>user_name.txt
+echo ${passward}>>passward.txt
+
+
+
+echo "Thank you"
