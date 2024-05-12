02b30213ea9f0b1a76c4125e9f35d1027c9ba245
ter0ter0 <panicdepanic@gmail.com>
2024-05-12 23:59:24 +0000

    sukosidekita

--- quest/pw.sh
+++ quest/pw.sh
@@ -1,18 +1,27 @@
-#! /bin/bash
-
+#! /bin/bash  
 echo "パスワードマネージャーへようこそ！"
 
-read -p  "サービス名を入力してください:" service_name
+read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" str
 
-read -p "ユーザー名を入力してください"  user_name
+        case "$str" in
 
-read -p "パスワードを入力してください" passward
+        "Add Password")
+
+	 	read -p "サービス名を入力してください:" service_name
+
+		read -p "ユーザー名を入力してください:"  user_name
+
+		read -p "パスワードを入力してください:" passward
 
 
-echo ${service_name} >>service_name.txt
-echo ${user_name}>>user_name.txt
-echo ${passward}>>passward.txt
+		echo "${service_name}" >>service_name.txt
+		echo "${user_name}">>user_name.txt
+		echo "${passward}">>passward.txt
+
+        ;;
+esac
 
 
 
-echo "Thank you"
+
+
