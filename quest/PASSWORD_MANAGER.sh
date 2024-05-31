0052a3b29d3128cefa5b28b4382e47f9c1a6fe69
ter0ter0 <panicdepanic@gmail.com>
2024-05-31 05:26:45 +0000

    編集

--- quest/PASSWORD_MANAGER.sh
+++ quest/PASSWORD_MANAGER.sh
@@ -13,7 +13,14 @@ read -p "次の選択肢から入力してください(Add Password/Get Password
 		echo "${service_name}" >>service_name.txt
 		echo "${user_name}">>user_name.txt
 		echo "${password}">>password.txt
-		echo "${password}" | gpg --symmetric --cipher-algo AES256 --output "${service_name}_password.gpg"
+		echo "DEBUG: GPG encryption command will be executed now."
+		echo "${password}" | gpg --symmetric --cipher-algo AES256 --output "${service_name}_password.gpg" 2> gpg_error.log
+		if [ $? -eq 0 ]; then
+                echo "暗号化されたパスワードファイルが ${service_name}_password.gpg として保存されました。"
+	            else
+                echo "パスワードファイルの保存に失敗しました。"
+        	    fi
+
 		echo "パスワードの追加は成功しました。"
 
 		;;
